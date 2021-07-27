75bdba463b5b61214fecfdf6b334a37ecea6204f
RenFei <i@renfei.net>
2021-07-27 13:07:16 +0800

    feat: 增加全局消息通知

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from '@/router'
 import i18n from '@/i18n/i18n'
+import Message from '@/commponents/Message/index'
 
 Vue.config.productionTip = false
+Vue.prototype.$message = Message
 
 new Vue({
-  vuetify,
-  i18n,
-  router,
-  render: h => h(App),
+    vuetify,
+    i18n,
+    router,
+    render: h => h(App),
 }).$mount('#app')
